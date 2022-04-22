cccdd4567299335b4816567762e4698677baf380
Laisvūnas <ajcfromroots@gmail.com>
2022-04-22 23:42:30 +0300

    BLET

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { useWindowScroll } from '@vueuse/core'
+import { computed } from 'vue'
+import { useScroll } from '@vueuse/core'
 import { useUtils } from '@/composables/useUtils'
 import ConnectionData from '@/components/ConnectionData.vue'
 import UserProfile from '@/components/UserProfile.vue'
@@ -7,19 +8,21 @@ import bg1 from '@/assets/img/bg1.jpg'
 import bg2 from '@/assets/img/bg2.jpg'
 import bg3 from '@/assets/img/bg3.jpg'
 
-const scroll = useWindowScroll()
+const scroll = useScroll(window)
 const { randomNumber } = useUtils()
 
 const bgArray = [bg1, bg2, bg3]
 const randomBackground = bgArray[randomNumber(3) - 1]
+
+const bgStyle = computed(() => ({
+  backgroundSize: 'cover',
+  backgroundImage: `url(${randomBackground})`,
+  backgroundPositionY: `${scroll.y.value * 0.8}px`
+}))
 </script>
 
 <template>
-  <div class="min-h-screen" :style="{
-    backgroundSize: 'cover',
-    backgroundImage: `url(${randomBackground})`,
-    backgroundPositionY: `${scroll.y} px`
-  }">
+  <div class="min-h-screen bg-no-repeat" :style="bgStyle">
     <UserProfile />
     <main class="mt-[40px] max-w-[1280px] mx-auto px-4">
       {{ scroll }}
