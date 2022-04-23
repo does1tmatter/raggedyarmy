d5cfb297bf1b614394b3307d405f7009a62802d3
Laisvūnas <ajcfromroots@gmail.com>
2022-04-24 00:07:42 +0300

    quickfix

--- src/components/MenuIcon.vue
+++ src/components/MenuIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="menuOpen ? '-translate-x-[6vw] sm:-translate-x-[8.3vw]' : ''">
+  <button>
     <div :class="[ menuOpen ? 'w-[30px] h-[4px] bg-white rounded-lg transition-all duration-500 rotate-45 translate-y-2' : 'w-[30px] h-[4px] bg-redish-300 rounded-lg transition-all duration-500' ]" />
     <div :class="[ menuOpen ? 'w-[30px] h-[4px] bg-white rounded-lg transition-all duration-500 mt-1 bg-transparent' : 'w-[30px] h-[4px] bg-redish-300 rounded-lg transition-all duration-500 mt-1' ]" />
     <div :class="[ menuOpen ? 'w-[30px] h-[4px] bg-white rounded-lg transition-all duration-500 mt-1 -rotate-45 -translate-y-2' : 'w-[30px] h-[4px] bg-redish-300 rounded-lg transition-all duration-500 mt-1' ]" />
